f55f7c6b26c819955fe0ff537498c1041406376f
Thorsten Göckeler <thorsten@goeckeler.com>
2018-04-12 02:58:04 +0200

    Simply added reader for gig data.

--- 00-festival-application/src/main/java/com/goeckeler/excercises/festival/Application.java
+++ 00-festival-application/src/main/java/com/goeckeler/excercises/festival/Application.java
@@ -1,37 +1,39 @@
 package com.goeckeler.excercises.festival;
 
-import com.goeckeler.excercises.festival.database.model.BandData;
-import com.goeckeler.excercises.festival.database.factory.DataFactory;
-import com.goeckeler.excercises.festival.tabular.reader.excel.ExcelReader;
-import com.goeckeler.excercises.festival.tabular.sheet.Table;
-import org.apache.commons.lang3.StringUtils;
-
 import java.io.FileNotFoundException;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+
+import com.goeckeler.excercises.festival.database.factory.DataFactory;
+import com.goeckeler.excercises.festival.database.model.BandData;
+import com.goeckeler.excercises.festival.database.model.GigData;
+import com.goeckeler.excercises.festival.tabular.reader.excel.ExcelReader;
+import com.goeckeler.excercises.festival.tabular.sheet.Table;
+
 public class Application
 {
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
     System.out.println("Revival Festival");
 
-    ExcelReader reader = new ExcelReader("Festival.xlsx");
+    final ExcelReader reader = new ExcelReader("Festival.xlsx");
     try {
       System.out.println("-----------------------------------------------------------");
       System.out.println("BANDS");
       System.out.println("-----------------------------------------------------------");
 
-      Table bandSheet = reader.read("bands");
+      final Table bandSheet = reader.read("bands");
       // System.out.println(bandSheet);
 
-      List<BandData> bandData = DataFactory.createBandData(bandSheet);
+      final List<BandData> bandData = DataFactory.createBandData(bandSheet);
       System.out.println(StringUtils.join(bandData, "\n"));
 
       System.out.println("-----------------------------------------------------------");
       System.out.println("PLAYLIST");
       System.out.println("-----------------------------------------------------------");
-      Table playlistSheet = reader.read("playlist");
-      System.out.println(playlistSheet);
-    } catch (FileNotFoundException ex) {
+      final List<GigData> gigs = DataFactory.createGigData(reader.read("playlist"));
+      System.out.println(gigs);
+    } catch (final FileNotFoundException ex) {
       System.err.println("Cannot find festival file.");
     }
   }
--- 00-festival-application/src/main/java/com/goeckeler/excercises/festival/database/factory/DataFactory.java
+++ 00-festival-application/src/main/java/com/goeckeler/excercises/festival/database/factory/DataFactory.java
@@ -1,11 +1,12 @@
 package com.goeckeler.excercises.festival.database.factory;
 
-import com.goeckeler.excercises.festival.database.model.BandData;
-import com.goeckeler.excercises.festival.tabular.sheet.Table;
-import com.goeckeler.excercises.festival.tabular.sheet.Row;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import com.goeckeler.excercises.festival.database.model.BandData;
+import com.goeckeler.excercises.festival.database.model.GigData;
+import com.goeckeler.excercises.festival.tabular.sheet.Row;
+import com.goeckeler.excercises.festival.tabular.sheet.Table;
 
 public class DataFactory
 {
@@ -14,16 +15,34 @@ public class DataFactory
    * }
    */
 
-  public static List<BandData> createBandData(Table sheet) {
+  public static List<BandData> createBandData(final Table sheet) {
     final int NAME_INDEX = 0;
     final int ARTISTS_INDEX = 1;
 
-    List<BandData> dataList = new ArrayList<>();
-    for (Row row : sheet) {
+    final List<BandData> dataList = new ArrayList<>();
+    for (final Row row : sheet) {
       // convert row to band data
-      String name = row.getCell(NAME_INDEX);
-      String artists = row.getCell(ARTISTS_INDEX);
+      final String name = row.getCell(NAME_INDEX);
+      final String artists = row.getCell(ARTISTS_INDEX);
       dataList.add(new BandData(name, artists));
+    }
+    // get rid of table header
+    dataList.remove(0);
+    return dataList;
+  }
+
+  public static List<GigData> createGigData(final Table sheet) {
+    final int ORDER_INDEX = 0;
+    final int SONG_INDEX = 1;
+    final int ACT_INDEX = 2;
+
+    final List<GigData> dataList = new ArrayList<>();
+    for (final Row row : sheet) {
+      // convert row to gig data
+      final String order = row.getCell(ORDER_INDEX);
+      final String song = row.getCell(SONG_INDEX);
+      final String act = row.getCell(ACT_INDEX);
+      dataList.add(new GigData(order, song, act));
     }
     // get rid of table header
     dataList.remove(0);
