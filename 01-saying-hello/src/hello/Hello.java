560a96663329ffbd543d69bbcf1a7616e6f1fdcc
Timo Göckeler <timo@goeckeler.com>
2018-11-18 12:49:57 +0100

    All kind of loops.

--- 01-saying-hello/src/hello/Hello.java
+++ 01-saying-hello/src/hello/Hello.java
@@ -3,14 +3,53 @@ package hello;
 public class Hello
 {
   public static void main(final String[] arguments) {
-    System.out.print("Hallo, ");
+    if (arguments.length == 0) {
+      System.out.println(hello(null));
+      return;
+    } 
+    
+    forLoop(arguments);
+    forEach(arguments);
+    whileLoop(arguments);
+    repeatLoop(arguments);
+  }
+  
+  private static void forLoop(String[] arguments) {
+    for(int i = 0; i < arguments.length; ++i) {
+      System.out.println(hello(arguments[i]));
+    }
+  }
+  
+  private static void forEach(String[] arguments) {
+    for (String name : arguments) {
+      System.out.println(hello(name));
+    }
+  }
+  
+  private static void whileLoop(String[] arguments) {
+    int index = 0;
+    while (index < arguments.length) {
+      System.out.println(hello(arguments[index++]) + " Have a nice day!");
+    }
+  }
+  
+  private static void repeatLoop(String[] arguments) {
+    int index = 0;
+    do {
+      System.out.println(hello(arguments[index]) + " Yo!");
+      index += 1;
+    } while (index < arguments.length);
+  }
+  
+  public static String hello(String name) {
+    StringBuilder greeting = new StringBuilder("Hallo, ");
 
-    if (arguments.length > 0) {
-      System.out.print(arguments[0]);
+    if (name == null || name.isEmpty()) {
+      greeting.append("Du");
     } else {
-      System.out.print("Du");
+      greeting.append(name);
     }
 
-    System.out.println("!");
+    return greeting.append("!").toString();
   }
 }
